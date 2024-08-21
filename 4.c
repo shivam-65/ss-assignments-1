25c5d9459770ff48157a32bc3990d63b3e4c4dea
shivam-65 <Padaliya.Prafulbhai@iiitb.ac.in>
2024-08-21 16:24:08 +0530

    open existing file

---
+++ 4.c
@@ -0,0 +1,18 @@
+/*
+Name: Padaliya Shivam
+Roll No.: MT2024107
+Program :4. Write a program to open an existing file with read write mode. Try O_EXCL flag also.
+*/
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(){
+	int f=open("three.txt",O_RDWR);
+	printf("%d\n",f);
+	int n=open("three.txt",O_EXCL);
+	printf("%d\n",n);
+ 
+}
+
