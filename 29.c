e4e5d6264daa034f5534adae3df31cf6fb5f7f90
shivam-65 <Padaliya.Prafulbhai@iiitb.ac.in>
2024-08-31 11:32:59 +0530

    change scheduling policy

---
+++ 29.c
@@ -0,0 +1,96 @@
+/*
+Name : Padaliya Shivam
+Roll No. : MT2024107
+Program :29. Write a program to get scheduling policy and modify the scheduling policy (SCHED_FIFO,
+SCHED_RR).
+*/
+
+#include <stdio.h>
+#include <sched.h>
+#include <unistd.h>
+int main()
+{
+    int pid = getpid();
+    // getting  scheduling policy of main process
+    int sched_policy = sched_getscheduler(pid);
+    if (sched_policy == -1)
+    {
+        perror("error\n");
+    }
+    else
+    {
+        switch (sched_policy)
+        {
+        case SCHED_FIFO:
+            printf("Current scheduling policy: SCHED_FIFO\n");
+            break;
+        case SCHED_OTHER:
+            printf("Current scheduling policy: SCHED_OTHER \n");
+            break;
+        case SCHED_RR:
+            printf("Current scheduling policy: SCHED_RR \n");
+            break;
+        default:
+            break;
+        }
+    }
+    int choice;
+    printf("To change scheduling policy select\n1:SCHED_FIFO\n2:SCHED_RR\n3:SCHED_OTHER\n");
+    scanf("%d", &choice);
+    struct sched_param s;
+    switch (choice)
+    {
+    case 1:
+        s.sched_priority = 99;
+        sched_setscheduler(pid, SCHED_FIFO, &s);
+        break;
+    case 2:
+        s.sched_priority = 99;
+        sched_setscheduler(pid, SCHED_RR, &s);
+        break;
+    case 3:
+        s.sched_priority = 0;
+        sched_setscheduler(pid, SCHED_OTHER, &s);
+        break;
+    default:
+        perror("invalid choice");
+        break;
+    }
+
+    sched_policy = sched_getscheduler(pid);
+    if (sched_policy == -1)
+    {
+        perror("error\n");
+    }
+    else
+    {
+        switch (sched_policy)
+        {
+        case SCHED_FIFO:
+            printf("Current scheduling policy: SCHED_FIFO\n");
+            break;
+        case SCHED_OTHER:
+            printf("Current scheduling policy: SCHED_OTHER \n");
+            break;
+        case SCHED_RR:
+            printf("Current scheduling policy: SCHED_RR \n");
+            break;
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
+
+/*
+shivam@shivam-Vostro-3590:~/Desktop/Handsonlist1$ sudo gcc 29.c
+shivam@shivam-Vostro-3590:~/Desktop/Handsonlist1$ sudo ./a.out
+Current scheduling policy: SCHED_OTHER 
+To change scheduling policy select
+1:SCHED_FIFO
+2:SCHED_RR
+3:SCHED_OTHER
+1
+Current scheduling policy: SCHED_FIFO
+*/
