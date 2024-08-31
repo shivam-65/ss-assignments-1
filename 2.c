ed65a9dd0844f304d51fc31082e79fedaa20e0b0
shivam-65 <Padaliya.Prafulbhai@iiitb.ac.in>
2024-08-31 12:25:09 +0530

    running in loop

--- 2.c
+++ 2.c
@@ -1,3 +1,10 @@
+/*
+Name : Padaliya Shivam
+Roll No. : MT2024107
+Program :2. Write a simple program to execute in an infinite loop at the background. Go to /proc directory and
+identify all the process related information in the corresponding proc directory.
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 
@@ -8,3 +15,16 @@ int main(){
 	}
 	return 0;
 }
+
+/*
+shivam@shivam-Vostro-3590:~/Desktop/Handsonlist1$ ./a.out
+Running in loop
+Running in loop
+Running in loop
+Running in loop
+Running in loop
+Running in loop
+Running in loop
+
+
+*/
